400ba965e5af7e322111c08a4a4852364b0d973d
Greg Ede <gregory.ede@gmail.com>
2024-01-20 10:58:46 +1300

    Cleaned up styling

--- packages/blenheim-client/src/app/timetable/feature/timetable-page/timetable-page.component.ts
+++ packages/blenheim-client/src/app/timetable/feature/timetable-page/timetable-page.component.ts
@@ -19,10 +19,14 @@ export class TimetablePageComponent {
   originCode = signal<Airport | undefined>(undefined);
   timetableList = signal<{ destination: Airport, destinationTimetables: TimetableFlight[] }[] | undefined>([]);
 
+  // Note when we change this page from one origin to another, the page instance does not change but the inputs will be updated which is why we use a setter to update the signals
+
+  // In the application configuration we set withComponentInputBinding() so that we can use @Input() in pull in the route parameter
   @Input() set origin(o: Airport) {
     this.originCode.set(o);
   }
 
+  // The route to this page includes a resolver that gets the timetable data from the server. We use @Input() to pull in that data
   @Input() set timetables(t: { destination: Airport, destinationTimetables: TimetableFlight[] }[]) {
     this.timetableList.set(t);
   }
