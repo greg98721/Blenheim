8b1af1ff5ac121c408857ecce967e08ccf009148
Greg Ede <gregory.ede@gmail.com>
2024-02-13 18:14:56 +1300

    Small fixes to package.json.lock

--- packages/blenheim-client/src/app/app.component.ts
+++ packages/blenheim-client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
