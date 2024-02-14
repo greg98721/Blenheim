c1b123a0c6f3d20ce3863db390c469f8ca2318c1
Greg Ede <gregory.ede@gmail.com>
2024-02-14 19:54:53 +1300

    Added user edit page

--- packages/blenheim-client/src/app/user/pages/user-edit-page/user-edit-page.component.ts
+++ packages/blenheim-client/src/app/user/pages/user-edit-page/user-edit-page.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { format } from 'date-fns/fp';
 
 @Component({
   selector: 'app-user-edit-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule, MatInputModule],
   templateUrl: './user-edit-page.component.html',
   styleUrl: './user-edit-page.component.scss'
 })
-export class UserEditPageComponent {
+export class UserEditPageComponent implements OnInit {
+  private _router = inject(Router);
+  private _fb = inject(FormBuilder);
 
+  private _userService = inject(UserService);
+  currentUser = this._userService.currentUser;
+
+  userForm = this._fb.nonNullable.group({
+    username: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    birthDate: ['', Validators.required],
+    address: ['', Validators.required],
+    email: ['', Validators.email],
+    phoneNumber: [''],
+  });
+
+  ngOnInit(): void {
+    if (this.currentUser !== undefined) {
+      this.userForm.patchValue({
+        username: this.currentUser()?.username,
+        firstName: this.currentUser()?.firstName,
+        lastName: this.currentUser()?.lastName,
+        birthDate: format('P', new Date()),
+        address: this.currentUser()?.address,
+        email: this.currentUser()?.email,
+        phoneNumber: this.currentUser()?.phoneNumber,
+      });
+    }
+  }
+
+  submitForm() {
+    if (this.userForm.valid) {
+      // this._userService.updateUser(this.userForm.value);
+      this._router.navigate(['/user']);
+    }
+  }
 }
